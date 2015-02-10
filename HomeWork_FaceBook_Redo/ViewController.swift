d409eada4e83163caa3a4f3eafb033c16eaa831e
tgulik <tgulik@yahoo-inc.com>
2015-02-09 18:52:25 -0800

    Initial Commit

---
+++ HomeWork_FaceBook_Redo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeWork_FaceBook_Redo
+//
+//  Created by Tomek Gulik on 2/9/15.
+//  Copyright (c) 2015 Tom Gulik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
